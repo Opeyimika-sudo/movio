48760349b4c358ae011ab0dc8b0887828bb5c434
Opeyimika-sudo <54380080+Opeyimika-sudo@users.noreply.github.com>
2023-05-30 16:45:34 +0100

    update: export useAppDispatch and useAppSelector

--- src/store.ts
+++ src/store.ts
@@ -8,6 +8,5 @@ export const store = configureStore({
     }
 })
 
-type RootState = ReturnType<typeof store.getState>;
 export const useAppDispatch: () => typeof store.dispatch=useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState>= useSelector;
+export const useAppSelector: TypedUseSelectorHook<ReturnType<typeof store.getState>>= useSelector;
